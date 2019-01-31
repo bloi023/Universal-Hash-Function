bc4f61f7e9b2a95057bb7eb85f1c6a1989349ab8
Eric Kim <dongjunkim@umail.ucsb.edu>
2019-01-30 20:18:33 -0800

    implemented finding the smallest prime larger than given

--- prog1B.cpp
+++ prog1B.cpp
@@ -1,41 +1,71 @@
 #include <iostream>
 #include "HashTable.h"
 #include <fstream>
+#include <vector>
+
 using namespace std;
 
 int main(){
 
-        std::string s_size;
-        cin >> s_size;
-        int truesize = std::stoi( s_size );
+  //Convert Input Size into Integer
+  std::string s_size;
+  cin >> s_size;
+  int input_size = std::stoi( s_size );
 
-	cout << truesize;
-	
-        HashTable t( 257 );
+  //Find Smallest Prime larger than input size
+  std::vector<int> primelist;
+  primelist.push_back(2);
+  primelist.push_back(3);
+  primelist.push_back(5);
+  primelist.push_back(11);
+  primelist.push_back(17);
+  primelist.push_back(37);
+  primelist.push_back(67);
+  primelist.push_back(131);
+  primelist.push_back(257);
+  primelist.push_back(521);
+  primelist.push_back(1031);
+  primelist.push_back(2053);
+  primelist.push_back(4099);
+  primelist.push_back(8209);
+  primelist.push_back(16411);
+  primelist.push_back(32771);
+  primelist.push_back(65537);
+  primelist.push_back(131101);
+  primelist.push_back(262147);
+  primelist.push_back(524309);
 
-        std::string next;
-        while( cin >> next ) {
-          if( next == "stat" )
-            t.stat();
-          else if( next == "insert" ) {
-            cin >> next;
-            t.insert(next);
-          }
-          else if( next == "delete" ) {
-            cin >> next;
-            t.remove(next);
-          }
-          else if( next == "lookup" ) {
-            cin >> next;
-            if( t.exists(next) )
-              cout << next << ": found" << endl;
-            else
-              cout << next << ": not found" << endl;
-          }
+  int truesize = 0;
+  int i = 0;
+  while( input_size > primelist[i] ) {
+    truesize = primelist[i+1];
+    i++;
+  }
+  
+  HashTable t( truesize );
+  
+  std::string next;
+  while( cin >> next ) {
+    if( next == "stat" )
+      t.stat();
+    else if( next == "insert" ) {
+      cin >> next;
+      t.insert(next);
+    }
+    else if( next == "delete" ) {
+      cin >> next;
+      t.remove(next);
+    }
+    else if( next == "lookup" ) {
+      cin >> next;
+      if( t.exists(next) )
+        cout << next << ": found" << endl;
+      else
+        cout << next << ": not found" << endl;
+    }
+  }
 
-        }
-
-        //      t.print();
-        return 0;
+  //      t.print();
+  return 0;
 
 }
